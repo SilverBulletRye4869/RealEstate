eaadb84123a2c2e58b6f7ac60ba62be989bcfa1e
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-11-04 17:42:58 +0900

    ymlの対応化

--- src/main/java/silverassist/realestate/RealEstate.java
+++ src/main/java/silverassist/realestate/RealEstate.java
@@ -9,11 +9,20 @@ import silverassist.realestate.event.NormalEvent;
 
 public final class RealEstate extends JavaPlugin {
     public static JavaPlugin plugin = null;
+    public static CustomConfig land = null;
+    public static CustomConfig city = null;
+    
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+        land = new CustomConfig(this, "land.yml");
+        city = new CustomConfig(this,"city.yml");
+
         this.saveDefaultConfig();
+        land.saveDefaultConfig();
+        city.saveDefaultConfig();
+
         PluginCommand command = getCommand("realestate");
         if(command!=null)command.setExecutor(new Normal());
         command = getCommand("realestate.admin");
@@ -28,5 +37,6 @@ public final class RealEstate extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        this.saveConfig();
     }
 }
