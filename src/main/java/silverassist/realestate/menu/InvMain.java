41a3ebb0080af6b11d3306b24569b6b6acfd9ee6
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-11-10 16:53:54 +0900

    設定GUI実装(途中)

--- src/main/java/silverassist/realestate/menu/InvMain.java
+++ src/main/java/silverassist/realestate/menu/InvMain.java
@@ -2,27 +2,96 @@ package silverassist.realestate.menu;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import silverassist.realestate.ActionType;
+import silverassist.realestate.RealEstate;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
-import static silverassist.realestate.Function.createItem;
-import static silverassist.realestate.Function.isOwner;
+import static silverassist.realestate.Function.*;
 import static silverassist.realestate.menu.InvFunc.*;
 
 public class InvMain {
     public static void openManageGui(Player p,String type){
-        List<String> data = splitType(type);
-        if(data.size()==1)return;
+        List<String> types = splitType(type);
+        if(types.size()==1)return;
         curInv.put(p,type);
+
         Inventory inv = null;
-        switch (data.get(1)){
+        int slot;
+        FileConfiguration region = RealEstate.region.getConfig();
+        String id = types.get(0);
+
+        switch (types.get(1)){
             case "def":
-                inv = Bukkit.createInventory(p,9,"§1§lid:"+data.get(0)+"§1§lの管理ページ");
-                inv.setItem(0,createItem(Material.GRASS_BLOCK,"§a§l土地の詳細設定",List.of("§f土地の状態を設定できます"),0));
+                inv = Bukkit.createInventory(p,9,"§1§lid:"+id+"§1§lの管理ページ");
+                inv.setItem(0,createItem(Material.GRASS_BLOCK,"§a§l土地の詳細設定",List.of("§f土地の情報を設定できます"),0));
                 inv.setItem(1,createItem(Material.PLAYER_HEAD,"§a§l土地の住人を管理",List.of("§f土地の住人を管理できます"),0));
-                if(isOwner(p,data.get(0)))inv.setItem(2,createItem(Material.DIAMOND_BLOCK,"§b§lオーナー権の譲渡",List.of("§fOwner権を譲渡することができます"),0));
+                if(isOwner(p,types.get(0)))inv.setItem(2,createItem(Material.DIAMOND_BLOCK,"§b§lオーナー権の譲渡",List.of("§fOwner権を譲渡することができます"),0));
+                break;
+            case "admin":
+                if(types.size()==2)return;
+                switch (types.get(2)){
+                    case "def":
+                        inv = Bukkit.createInventory(p,9,"§1§lid:"+id+"§1§lの詳細管理ページ");
+                        inv.setItem(0,createItem(Material.ENDER_PEARL,"§a§l土地のテレポート位置を設定",List.of("§e現在地を土地のテレポート地点にします","§c(土地の範囲外には設定できません)")));
+                        inv.setItem(1,createItem(Material.COMPARATOR,"§c§lデフォルトの権限を設定", List.of("§f全プレイヤーの権限を設定します")));
+                        if(isOwner(p,types.get(0))){
+                            inv.setItem(4,createItem(Material.GRAY_STAINED_GLASS_PANE,"§r",null));
+                            inv.setItem(5,createItem(Material.PAPER,"§b§l土地の状態を設定",List.of("§f土地の状態を設定できます","§6現在の状態: §a§l"+region.getString(id+".status"))));
+                            inv.setItem(6,createItem(Material.GOLD_INGOT,"§6§l土地の値段を設定",List.of("§f土地の値段を設定できます","§6現在の価格: §c§l"+region.getString(id+".price"))));
+                        }
+                        break;
+                    case "person":
+                        if(types.size()==3)return;
+                        String t3 = types.get(3);
+                        switch (t3){
+                            case "def":
+                                Set<String> uuidList = region.getConfigurationSection(id + ".user").getKeys(false);
+                                int size = Math.min(54, (((uuidList.size()-1)/9)+1) * 9); //人数に合わせて大きさを調整(54人まで対応)
+                                inv = Bukkit.createInventory(p,size,"§1§lid:"+id+"§1§lの住人管理");
+                                slot = 0;
+                                for(String uuidStr : uuidList){
+                                    UUID u = UUID.fromString(uuidStr);
+                                    if(p.getUniqueId().equals(u))return; //--デバック時アウト
+                                    ItemStack head = createItem(Material.PLAYER_HEAD,"§a§l"+Bukkit.getOfflinePlayer(u).getName(),List.of("§fuuid: "+u));
+                                    SkullMeta meta = (SkullMeta) head.getItemMeta();
+                                    meta.setOwningPlayer(Bukkit.getOfflinePlayer(u));
+                                    head.setItemMeta(meta);
+                                    inv.setItem(slot,head);
+                                    slot++;
+                                }
+                                break;
+                            default:
+                                UUID u = UUID.fromString(t3);
+                                inv = Bukkit.createInventory(p,18,"§1§lid:"+id+"の「"+Bukkit.getPlayer(u).getName()+"」の権限設定");
+                                for(int i : new int[]{0,1,7,8}){
+                                    inv.setItem(i,createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE,"§r",null));
+                                    inv.setItem(9+i,createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE,"§r",null));
+                                }
+                                inv.setItem(2,createItem(Material.GOLD_BLOCK,"§6§l管理(Admin)権限",List.of("§f下記を除くすべての行動が可能です","§6・Ownerの任命","§6・Adminの任命","§6・土地状態の変更","§6・土地の値段変更")));
+                                inv.setItem(3,createItem(Material.IRON_BLOCK,"§7§lフル活動権限",List.of("§f土地の管理行為を除く","§fすべての行動が可能です")));
+                                inv.setItem(4,createItem(Material.GRASS_BLOCK,"§a§lブロック権限",List.of("§fブロックの設置･破壊が可能です")));
+                                inv.setItem(5,createItem(Material.OAK_DOOR,"§e§l右クリック権限",List.of("§fチェスト系ブロック以外の","§fブロックに対して右クリック","§fを実行できます")));
+                                inv.setItem(6,createItem(Material.CHEST,"§e§lチェストアクセス権限",List.of("§fチェスト系ブロックへの","§fアクセスが可能です")));
+
+                                int permNum = region.getInt(id+".user."+u);
+                                String perm = new StringBuilder(Integer.toBinaryString(permNum)).reverse() + "0000000000";
+                                for(int i = 0;i< ActionType.values().length;i++){
+                                    if(perm.charAt(i)=='1')inv.setItem(11+i,createItem(Material.LIME_STAINED_GLASS_PANE,"§a§l許可",null));
+                                    else inv.setItem(11+i,createItem(Material.RED_STAINED_GLASS_PANE,"§c§l拒否",null));
+                                }
+                                if(!isOwner(p,id))inv.setItem(11,createItem(Material.BARRIER,"§c変更権限なし",null));
+                                inv.setItem(17,createItem(Material.LAVA_BUCKET,"§c§lユーザーを退去",List.of("§6ここをクリックして、","§6ユーザーを退去させる")));
+                        }
+                        break;
+                }
         }
         if(inv!=null)p.openInventory(inv);
         else p.sendMessage(type); //デバックメッセージ
