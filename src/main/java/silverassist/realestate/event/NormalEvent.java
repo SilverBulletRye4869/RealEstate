db807c8a867e614f4bd210dd4ba8152afe989ad4
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-11-13 19:48:14 +0900

    土地の看板を製作できるように修正

--- src/main/java/silverassist/realestate/event/NormalEvent.java
+++ src/main/java/silverassist/realestate/event/NormalEvent.java
@@ -1,18 +1,25 @@
 package silverassist.realestate.event;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import silverassist.realestate.ActionType;
+import silverassist.realestate.RealEstate;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static silverassist.realestate.Function.*;
 
@@ -34,8 +41,20 @@ public class NormalEvent implements Listener {
     @EventHandler
     public void breakBlock(BlockBreakEvent e){
         Player p = e.getPlayer();
-        if(p.isOp())return;
-        if(hasPermission(p,e.getBlock().getLocation(), ActionType.BLOCK))return;
+        Block block = e.getBlock();
+        if(hasPermission(p,block.getLocation(), ActionType.BLOCK)) {
+            if(!(block.getState() instanceof Sign))return;
+            Sign sign = (Sign) block.getState();
+            if(!sign.getLine(0).contains(PREFIX))return;
+            String id = sign.getLine(1).replace("§d§lid: ","");
+            FileConfiguration region = RealEstate.region.getConfig();
+            if(region.get(id)==null)return;
+            List<Location> locList =(List<Location>) region.getList(id+".sign");
+            locList.remove(e.getBlock().getLocation());
+            region.set(id+".sign",locList);
+            RealEstate.region.saveConfig();
+            return;
+        }
         sendPrefixMessage(p,"§cこのブロックを壊す権限がありません");
         e.setCancelled(true);
 
@@ -77,5 +96,32 @@ public class NormalEvent implements Listener {
         e.setCancelled(true);
     }
 
+    private final Pattern REGION_SIGN = Pattern.compile("id:[0-9]*");
+    @EventHandler
+    public void signPlaceEvent(SignChangeEvent e){
+        String[] line = e.getLines();
+        if(line.length==0)return;
+        if(!REGION_SIGN.matcher(line[0]).matches())return;
+        String id = line[0].replace("id:","");
 
+        FileConfiguration region = RealEstate.region.getConfig();
+        if(region.get(id)==null)return;
+        List<Location> locList = (List<Location>)region.getList(id + ".sign");
+        Player p = e.getPlayer();
+        if(!isAdmin(p,id)){
+            sendPrefixMessage(p,"§c管理権限を持っていない土地の看板は置けません！");
+            return;
+        }
+        if(locList.size()>2){
+            sendPrefixMessage(p,"§c土地の看板は3つまでしか認識されません");
+            return;
+        }
+        setRegionSign((Sign) e.getBlock().getState(), id);
+
+        Location loc = e.getBlock().getLocation();
+        broadCast(loc+"");
+        if(!locList.contains(loc))locList.add(e.getBlock().getLocation());
+        region.set(id+".sign",locList);
+        RealEstate.region.saveConfig();
+    }
 }
