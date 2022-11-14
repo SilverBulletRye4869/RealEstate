fa113da8d2c4622fd9689a78e856cc161910540a
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-11-14 20:10:53 +0900

    看板クリック時の情報表示 & 小修正

--- src/main/java/silverassist/realestate/event/NormalEvent.java
+++ src/main/java/silverassist/realestate/event/NormalEvent.java
@@ -1,9 +1,10 @@
 package silverassist.realestate.event;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -18,12 +19,13 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import silverassist.realestate.ActionType;
 import silverassist.realestate.RealEstate;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static silverassist.realestate.Function.*;
 
 public class NormalEvent implements Listener {
+
     //チェスト系ブロック
     public final List<Material> chestBlock = List.of(
             Material.BARREL,
@@ -80,10 +82,11 @@ public class NormalEvent implements Listener {
 
     //------------------------------------------------------------------------クリック系
     @EventHandler
+    //通常クリック
     public void interactEvent(PlayerInteractEvent e){
         if(e.getAction()!= Action.RIGHT_CLICK_BLOCK)return;
         if(!e.hasBlock())return;
-
+        if(e.getClickedBlock().getState() instanceof Sign)signClickEvent(e);
         Player p = e.getPlayer();
         if(p.isOp())return;
         Block block = e.getClickedBlock();
@@ -96,8 +99,13 @@ public class NormalEvent implements Listener {
         e.setCancelled(true);
     }
 
-    private final Pattern REGION_SIGN = Pattern.compile("id:[0-9]*");
+
+
+    //-------------------------------------------------------------------土地看板
+    private final Pattern REGION_SIGN = Pattern.compile("id:\\d*");
+
     @EventHandler
+    //看板製作
     public void signPlaceEvent(SignChangeEvent e){
         String[] line = e.getLines();
         if(line.length==0)return;
@@ -106,7 +114,16 @@ public class NormalEvent implements Listener {
 
         FileConfiguration region = RealEstate.region.getConfig();
         if(region.get(id)==null)return;
-        List<Location> locList = (List<Location>)region.getList(id + ".sign");
+        List<Location> locList = new ArrayList<>((List<Location>)region.getList(id + ".sign"));
+
+        //看板が無いのに残っているか確認
+        new ArrayList<>(locList).forEach(loc -> { //new ArrayListしないとエラー吐く！
+                Block block = RealEstate.plugin.getServer().getWorld(loc.getWorld().getName()).getBlockAt(loc);
+                if(block==null)return;
+                BlockState state = block.getState();
+                if (state instanceof Sign && ((Sign) state).getLine(0).equals(PREFIX)) return;
+                locList.remove(loc);
+        });
         Player p = e.getPlayer();
         if(!isAdmin(p,id)){
             sendPrefixMessage(p,"§c管理権限を持っていない土地の看板は置けません！");
@@ -117,11 +134,40 @@ public class NormalEvent implements Listener {
             return;
         }
         setRegionSign((Sign) e.getBlock().getState(), id);
-
         Location loc = e.getBlock().getLocation();
-        broadCast(loc+"");
         if(!locList.contains(loc))locList.add(e.getBlock().getLocation());
         region.set(id+".sign",locList);
         RealEstate.region.saveConfig();
+    }
+
+    //看板クリック
+    private void signClickEvent(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+
+        Sign sign = (Sign)e .getClickedBlock().getState();
+        String[] lines = sign.getLines();
+        if(!lines[0].equals(PREFIX))return;
+        String id = lines[1].replace("§d§lid: ","");
+        FileConfiguration region = RealEstate.region.getConfig();
+        if(region.get(id)==null)return;
+
+        sendPrefixMessage(p,"§e§l-------[id:"+id+"の情報]-------");
+        OfflinePlayer owner = getOwner(id);
+        sendPrefixMessage(p,"§6§l所有者: "+lines[2]);
+        sendPrefixMessage(p,"§6§lステータス: §a§l"+lines[3]);
+        sendPrefixMessage(p,"§6§l価格: §a§l"+region.get(id+".price")+RealEstate.plugin.getConfig().get("money_unit"));
+        Set<String> members = new HashSet<>();
+        if(region.getConfigurationSection(id+".user") != null)members = region.getConfigurationSection(id+".user").getKeys(false);
+        sendPrefixMessage(p,"§6§l住人の数: §a§l"+members.size()+"人");
+        if(p.isOp()){
+            Iterator members_iterator = members.iterator();
+            int cnt = 1;
+            while (members_iterator.hasNext()){
+                String target = members_iterator.next().toString();
+                sendPrefixMessage(p,"§c§l住人("+cnt+"): §a§l"+getPlayer(target).getName()+"§d§l["+region.get(id+".user."+target)+"]");
+                cnt++;
+            }
+        }
+        sendPrefixMessage(p,"§e§l--------------------------------------");
     }
 }
