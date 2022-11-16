647a24ab6962ea99dd10860ea8e17d8e12228d98
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-11-16 21:07:56 +0900

    小修整

--- src/main/java/silverassist/realestate/menu/InvFunc.java
+++ src/main/java/silverassist/realestate/menu/InvFunc.java
@@ -18,18 +18,19 @@ import static silverassist.realestate.Function.createItem;
 
 public class InvFunc {
     static Map<Player,String> curInv = new HashMap<>();
+    static final ItemStack ENABLED_TRUE_ITEM = createItem(Material.GREEN_CONCRETE, "§a保護有効化中",List.of("§fこのワールドに属する","§f全ての保護が§6有効化§fさ","§fれています"));
+    static final ItemStack ENABLED_FALSE_ITEM = createItem(Material.RED_CONCRETE, "§c保護無効化中",List.of("§fこのワールドに属する","§f全ての保護が§6無効化§fさ","§fれています"));
 
     static List<String> splitType(String type){
         return List.of(type.split("\\."));
     }
 
-
     static final int START_PERM_PLACE = 10; //権限設定用板ガラスが一番初めに登場する位置
-    static void SetPermission(InventoryClickEvent e, FileConfiguration region, String path){
+    static void SetPermission(InventoryClickEvent e, FileConfiguration ymlFile, String path){
         int slot = e.getSlot();
         if(slot<START_PERM_PLACE || slot>START_PERM_PLACE+ActionType.values().length-1)return;
         int permPlace = slot - START_PERM_PLACE;
-        int permNum = region.getInt(path);
+        int permNum = ymlFile.getInt(path);
         if(e.getCurrentItem().getType()== Material.LIME_STAINED_GLASS_PANE){
             permNum -= Math.pow(2,permPlace);
             e.getInventory().setItem(slot,createItem(Material.RED_STAINED_GLASS_PANE,"§c§l拒否",null));
@@ -37,7 +38,7 @@ public class InvFunc {
             permNum += Math.pow(2,permPlace);
             e.getInventory().setItem(slot,createItem(Material.LIME_STAINED_GLASS_PANE,"§a§l許可",null));
         }
-        region.set(path,permNum);
+        ymlFile.set(path,permNum);
         return;
     }
 
@@ -57,10 +58,14 @@ public class InvFunc {
         inv.setItem(7,createItem(Material.HOPPER,"§1§lPICKUP権限",List.of("§f保護エリア内で落ちている","§fアイテムを拾うことがで","§fきます。")));
 
         int permNum;
-        FileConfiguration region = RealEstate.region.getConfig();
-        if(targetUUID.equals("admin"))permNum = region.getInt(id+".default");
-        else permNum = region.getInt(id+".user."+targetUUID);
-
+        if(targetUUID != null) {  //対象プレイヤーが記載されいるときは土地の権限を見る
+            FileConfiguration region = RealEstate.region.getConfig();
+            if (targetUUID.equals("default")) permNum = region.getInt(id + ".default");
+            else permNum = region.getInt( id + ".user." + targetUUID);
+        }else{  //対象プレイヤーが記載されいるときはワールドの権限を見る
+            FileConfiguration world = RealEstate.world.getConfig();
+            permNum = world.getInt(id+".permission");
+        }
         String perm = new StringBuilder(Integer.toBinaryString(permNum)).reverse() + "0000000000";
         for(int i = 0; i< ActionType.values().length; i++){
             if(perm.charAt(i)=='1')inv.setItem(START_PERM_PLACE+i,createItem(Material.LIME_STAINED_GLASS_PANE,"§a§l許可",null));
